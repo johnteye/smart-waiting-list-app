d74bbda3d50abdf3c6381152dd9e8bf7d560f60c
sonnysam <samuelagbenyo067@gmail.com>
2023-03-28 21:10:10 +0000

    app name changed and changes made to node_modules

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,12 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import tw from "twrnc";
 
-export default function Onboarding() {
+import Onboarding from "react-native-onboarding-swiper";
+
+const Done = ({ ...props }) => (
+  <TouchableOpacity {...props} style={{ marginHorizontal: 20 }}>
+    <Text style={{ fontSize: 16, fontWeight: "bold" }}>Done</Text>
+  </TouchableOpacity>
+);
+
+export default function OnBoarding({ navigation }) {
   return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
-  )
+    <Onboarding
+      DoneButtonComponent={Done}
+      onDone={() => navigation.push("GetStarted")}
+      onSkip={() => navigation.push("Login")}
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("../assets/images/people3.png")}
+              style={{ width: 220, height: 245 }}
+            />
+          ),
+          title: "Create a free account",
+          subtitle: "New here, start by signing up",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("../assets/images/woman.webp")}
+              style={{ width: 220, height: 245 }}
+            />
+          ),
+          title: "Calculate tithe in App",
+          subtitle: "Not sure about tithe?",
+        },
+        // {
+        //   backgroundColor: "#fff",
+        //   image: (
+        //     <Image
+        //       source={require("../assets/online-pay.png")}
+        //       style={{ width: 305, height: 245 }}
+        //     />
+        //   ),
+        //   title: "Pay tithes & offerings online",
+        //   subtitle: "Pay with ease with few clicks",
+        // },
+      ]}
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
